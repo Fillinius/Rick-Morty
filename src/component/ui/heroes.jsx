d2a2c4306ab9387e306ca4f8c41e5e0694d52250
Vladimir Revyagin <skorabeyf@gmail.com>
2024-07-05 08:48:57 +0300

    chore(hero): компановка данных

--- src/component/ui/heroes.jsx
+++ src/component/ui/heroes.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import useFetch from '../../hooks/useFetch';
 import '../../../src/index.css'
-import HeroesList from '../page/heroesList';
+import HeroesList from '../page/heroes/heroesList';
 
 const Heroes = () => {
   const { pathname } = useLocation()
