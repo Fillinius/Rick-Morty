9022077945530da64d7abe85b132fa4fd8db468c
Vladimir Revyagin <skorabeyf@gmail.com>
2024-07-17 13:09:51 +0300

    feat(infinityscroll):Работает не совсем корректно

--- src/component/ui/heroes.jsx
+++ src/component/ui/heroes.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import '../../../src/index.css'
 import HeroesList from '../page/heroes/heroesList';
@@ -9,31 +9,41 @@ const Heroes = () => {
   const { pathname } = useLocation()
   const endPoint = pathname
 
-  const [query, setQuery] = useState('')
-  const [pageNumber, setPageNumber] = useState('')
+  const [pageNumber, setPageNumber] = useState(1)
   const { data,
     isLoading,
     error,
     hasMore,
-  } = useSearchData(query, pageNumber, endPoint)
+  } = useSearchData(pageNumber, endPoint)
 
-  const handleFind = (event) => {
-    setQuery(event.target.value)
-    setPageNumber(1)
-  }
   const handleAsc = () => {
-    setPageNumber(p => Number(p) + 5)
+    setPageNumber(p => p < 42 ? Number(p) + 2 : p)
   }
   const handleDesc = () => {
-    setPageNumber(p => Number(p) - 5)
+    setPageNumber(p => p > 0 ? Number(p) - 2 : p)
   }
-  console.log(query);
+
+  const observer = useRef()
+  const lastNodeRef = useCallback((node) => {
+    console.log(node);
+    if (isLoading) return;
+    if (observer.current) {
+      observer.current.disconnect()
+    }
+    observer.current = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting && hasMore) {
+        setPageNumber((prev) => prev + 1)
+        console.log('visible')
+      }
+    })
+    if (node) {
+      observer.current.observe(node)
+    }
+  }, [isLoading, hasMore])
+
   return (<>
     {error && (<p>- Ошибка получения данных</p>)}
     {isLoading && (<h1>Загрузка данных</h1>)}
-    <div className='find'>
-      <input type="text" onChange={handleFind} />
-    </div>
     <div>
       <button onClick={handleAsc}>Next</button>
       <button onClick={handleDesc}>Previos</button>
@@ -41,7 +51,7 @@ const Heroes = () => {
     {data && !isLoading && !error && (
       data.length === 0
         ? 'Список пуст'
-        : <HeroesList data={data} />)}
+        : <HeroesList data={data} lastNodeRef={lastNodeRef} />)}
   </>);
 }
 
--- src/hooks/useSearchData.js
+++ src/hooks/useSearchData.js
@@ -2,44 +2,48 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { BASEURL } from '../path/internalPaths.ts'
 
-const useSearchData = (query, pageNumber, endPoint) => {
+const useSearchData = (pageNumber, endPoint) => {
   const [data, setData] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState(false)
   const [hasMore, setHasMore] = useState(false)
+  console.log(data)
 
-  useEffect(() => {
-    setData([])
-  }, [query])
+  const uniqueData = (array) =>
+    array.reduce((accumulator, current) => {
+      if (accumulator.findIndex((object) => object.id === current.id) === -1) {
+        accumulator.push(current)
+      }
+      return accumulator
+    }, [])
 
   useEffect(() => {
     setIsLoading(true)
     setError(false)
     let cancel
-    axios(
-      {
-        method: 'GET',
-        url: BASEURL.base + endPoint,
-        params: { q: query, page: pageNumber },
-        cancelToken: new axios.CancelToken((c) => (cancel = c)),
-      },
-      [query, pageNumber]
-    )
+    axios({
+      method: 'GET',
+      url: BASEURL.base + endPoint,
+      params: { page: pageNumber },
+      cancelToken: new axios.CancelToken((c) => (cancel = c)),
+    })
       .then((res) => {
-        setData(res.data.results)
-
-        // setData((p) => {
-        //   return [...new Set([...p, ...res.data.results])]
-        // })
+        console.log(res.data)
+        setData((p) => {
+          return uniqueData([...p, ...res.data.results])
+        })
         setHasMore(res.data.results.length > 0)
         setIsLoading(false)
       })
       .catch((e) => {
-        if (axios.isCancel(e)) return
+        if (axios.isCancel(e)) {
+          return
+        }
         setError(false)
         console.log('errAxios', e)
       })
-  }, [query, pageNumber])
+    return () => cancel()
+  }, [pageNumber])
 
   const getDataId = (id) => {
     const itemId = data.find((item) => {
